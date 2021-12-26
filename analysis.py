2bbd9516ad688b5cb0cfcd06842c33c7a3e2b5e4
jeff <hongli@Hongs-iMac.local>
2021-12-26 11:08:53 -0600

    add addition pandas testing cases

--- analysis.py
+++ analysis.py
@@ -16,7 +16,25 @@ print(poke['Name'][0:20])
 print(poke[['Name', 'Type 1', 'HP']][0:20])
 
 # Read each rows (top 4 rows)
+print('-------top 4----------')
 print(poke.head(4))
 
 # Read a specific location
+print('-------iloc 1----------')
 print(poke.iloc[1])
+
+# Read multiple rows
+print('-------iloc 1-4 -------')
+print(poke.iloc[1:4])
+
+# Read specific location
+print(poke.iloc[2,1])
+
+# Loop through and read each row
+print('-------loop read each row -------')
+for index, row in poke.iterrows():
+    print(index, row[['Name', 'HP']])
+
+##
+print('-------look for rows with condition -------')
+print(poke.loc[poke['Type 1'] == "Fire"])
